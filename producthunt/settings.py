b9594b866a1da7b413f0f889c2d922cb8f3a83af
Redysz <tuliokkj@gmail.com>
2023-02-25 17:01:12 +0100

    Add ignored settings

--- producthunt/settings.py
+++ producthunt/settings.py
@@ -145,3 +145,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("Jesteś na swoim komputerze lokalnym")
